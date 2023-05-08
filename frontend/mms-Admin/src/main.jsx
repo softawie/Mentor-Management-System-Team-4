4acc52ccf8d2eee35c09e4a7d0b72719872e3a25
Yahya Zakaria Taha <ytaha@emkanfinco.com.sa>
2023-05-08 23:23:06 +0300

    fix:clickJacking

--- frontend/mms-Admin/src/main.jsx
+++ frontend/mms-Admin/src/main.jsx
@@ -9,19 +9,27 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import StatusPage from "./components/statusPage/status-page.component";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ToastContainer />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>
-);
+if (window.self === window.top) {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <ToastContainer />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <StatusPage code="403" title={"403"} helperText={""} btnText={""} />,
+    document.getElementById("root")
+  );
+}
