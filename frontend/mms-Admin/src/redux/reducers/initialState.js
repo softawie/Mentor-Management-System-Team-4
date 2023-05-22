84075b598763fc723249ae335ffe802d773f65b4
Yahya Zakaria Taha <ytaha@emkanfinco.com.sa>
2023-05-22 10:50:16 +0300

    fix bugs

--- frontend/mms-Admin/src/redux/reducers/initialState.js
+++ frontend/mms-Admin/src/redux/reducers/initialState.js
@@ -1,8 +1,5 @@
 import { REDIRECTION_TYPE } from "../../utils/constants";
 
-const isLogin = JSON.parse(localStorage.getItem("IS_LOGIN"));
-const token = localStorage.getItem("token");
-
 const initialState = {
   isLoggedIn: false,
   isLoading: false,
