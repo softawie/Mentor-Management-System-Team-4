d3c63e250d5ad9f3648d3b5f4899bb7805aadbd9
Raoul KUATE <rkuate1050@gmail.com>
2023-05-24 04:28:43 +0100

    feat: update setting model

---
+++ backend/database/migrations/20230524015704-add-userid-on-setting.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface,Sequelize) {
+    queryInterface.addColumn(
+      "settings",
+      "user_id",
+      {
+        type: Sequelize.DataTypes.BIGINT
+      }
+    )
+  },
+  async down (queryInterface) {
+    queryInterface.removeColumn("settings","user_id")
+  }
+};
