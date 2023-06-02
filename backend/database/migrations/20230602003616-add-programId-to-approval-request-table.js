714dcf1277b687f1e17616ec8ed669344fa44132
raoularavel <rkuate1050@gmail.com>
2023-06-02 01:45:17 +0100

    :sparkles: feat: Add program ID to Approval Request Table
    Approval request table is used for program requests

---
+++ backend/database/migrations/20230602003616-add-programId-to-approval-request-table.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('approval_requests', 'program_id', {
+      type: Sequelize.DataTypes.BIGINT,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('approval_requests', 'program_id');
+  },
+};
