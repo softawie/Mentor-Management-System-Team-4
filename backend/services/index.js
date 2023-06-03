14f4d7b06096e1779c34f2f0237b76187d1d44ba
raoularavel <rkuate1050@gmail.com>
2023-06-03 23:00:59 +0100

    :sparkles: feat: Create task controller
    Methods : create, findById, update, destroy, findAll, findAllByUserId, findAssignedUsersByTask

--- backend/services/index.js
+++ backend/services/index.js
@@ -5,5 +5,15 @@ import * as tokenService from './token.service';
 import * as notificationService from './notification.service';
 import * as ApprovalRequestService from './approval-request.service';
 import * as programService from './program.service';
+import * as taskService from './task.service';
 
-export { authService, emailService, userService, tokenService, notificationService, ApprovalRequestService, programService };
+export {
+  authService,
+  emailService,
+  userService,
+  tokenService,
+  notificationService,
+  ApprovalRequestService,
+  programService,
+  taskService,
+};
